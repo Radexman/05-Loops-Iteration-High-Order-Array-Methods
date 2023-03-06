a66822f21a7c9a8b0c7468d1ff903b529f9ffb82
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-06 19:29:36 +0100

    Break & Continue
    Finished Break and Continue statements lesson

---
+++ 06-break-and-continue/script.js
@@ -0,0 +1,17 @@
+// Break Statement
+for (let i = 0; i <= 20; i++) {
+	if (i === 15) {
+		console.log('Break...');
+		break;
+	}
+	console.log(i);
+}
+
+// Continue Statement
+for (let i = 0; i <= 20; i++) {
+	if (i === 13) {
+		console.log("I'm superstitius!");
+		continue;
+	}
+	console.log(i);
+}

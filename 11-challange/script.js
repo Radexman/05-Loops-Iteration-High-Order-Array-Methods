0e0bc59ad19a6630cb6a7237ba062b3e8b8b8256
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-10 13:09:38 +0100

    Iteration Challanges
    Managed to complete challanges from the course

--- 11-challange/script.js
+++ 11-challange/script.js
@@ -5,35 +5,35 @@ const people = [
 		firstName: 'John',
 		lastName: 'Doe',
 		email: 'john@gmail.com',
-		phone: '111-111-1111',
+		phone: '111-111-111',
 		age: 30,
 	},
 	{
 		firstName: 'Jane',
 		lastName: 'Poe',
 		email: 'jane@gmail.com',
-		phone: '222-222-2222',
+		phone: '222-222-222',
 		age: 25,
 	},
 	{
 		firstName: 'Bob',
 		lastName: 'Foe',
 		email: 'bob@gmail.com',
-		phone: '333-333-3333',
+		phone: '333-333-333',
 		age: 45,
 	},
 	{
 		firstName: 'Sara',
 		lastName: 'Soe',
-		email: 'Sara@gmail.com',
-		phone: '444-444-4444',
+		email: 'sara@gmail.com',
+		phone: '444-444-444',
 		age: 19,
 	},
 	{
 		firstName: 'Jose',
 		lastName: 'Koe',
 		email: 'jose@gmail.com',
-		phone: '555-555-5555',
+		phone: '555-555-555',
 		age: 23,
 	},
 ];
@@ -53,5 +53,14 @@ console.log(youngPeople);
 
 const numbers = [2, -30, 50, 20, -12, -9, 7];
 
-const positive = numbers.filter((num) => num > 0).reduce((acc, cur) => (acc += cur));
-console.log(positive);
+const positiveSum = numbers.filter((number) => number >= 0).reduce((acc, cur) => (acc += cur));
+
+console.log(positiveSum);
+
+// Challange 3
+
+const words = ['coder', 'programmer', 'developer'];
+
+const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+
+console.log(capitalizedWords);
